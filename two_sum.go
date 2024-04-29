ed0e89e2b06e9f69ff72b87f9482bbc7c1d3a3ac
patrick <patricksu2023@icloud.com>
2024-04-29 15:29:56 +0800

    two sum sorted array

--- two_sum.go
+++ two_sum.go
@@ -41,3 +41,70 @@ func TwoSum(array []int, target int) []int {
 
 	panic("no solution")
 }
+
+// TwoSumSortedArray .
+// Given a 1-indexed array of integers numbers that is already sorted in non-decreasing order, find two numbers such that they add up to a specific target number. Let these two numbers be numbers[index1] and numbers[index2] where 1 <= index1 < index2 <= numbers.length.
+// Return the indices of the two numbers, index1 and index2, added by one as an integer array [index1, index2] of length 2.
+// The tests are generated such that there is exactly one solution. You may not use the same element twice.
+// Your solution must use only constant extra space.
+// binary search
+func TwoSumSortedArray(array []int, target int) []int {
+	for idx, elm := range array {
+		otherElm := target - elm
+		otherIdx, ok := TwoSumSortedArray_binarySearch(array, otherElm, 0, len(array)-1, idx)
+		if !ok {
+			continue
+		}
+		return []int{idx, otherIdx}
+	}
+
+	panic("no solution")
+}
+
+func TwoSumSortedArray_binarySearch(array []int, target, left, right, exclude int) (int, bool) {
+	// stop condition
+	if left > right {
+		return 0, false
+	}
+
+	if left == exclude {
+		return TwoSumSortedArray_binarySearch(array, target, left+1, right, exclude)
+	}
+
+	if right == exclude {
+		return TwoSumSortedArray_binarySearch(array, target, left, right-1, exclude)
+	}
+
+	mid := (left + right) / 2
+	midElm := array[mid]
+	if target == midElm {
+		// found, but this element is used.
+		if mid == exclude {
+
+			// try search left
+			if targetIdx, ok := TwoSumSortedArray_binarySearch(array, target, left, mid-1, exclude); ok {
+				return targetIdx, ok
+			}
+
+			// try search right
+			if targetIdx, ok := TwoSumSortedArray_binarySearch(array, target, mid+1, right, exclude); ok {
+				return targetIdx, ok
+			}
+
+			// no found
+			return 0, false
+		}
+
+		return mid, true
+	}
+
+	if target > midElm {
+		// mid + 1 > left?
+		// mid >= left ---> mid + 1 > left
+		return TwoSumSortedArray_binarySearch(array, target, mid+1, right, exclude)
+	} else {
+		// mid - 1 < right ?
+		// mid <= right ---> mid - 1 < right
+		return TwoSumSortedArray_binarySearch(array, target, left, mid-1, exclude)
+	}
+}
--- two_sum_test.go
+++ two_sum_test.go
@@ -57,3 +57,56 @@ func TestTwoSum(t *testing.T) {
 		})
 	}
 }
+
+func TestTwoSumSortedArray(t *testing.T) {
+	type args struct {
+		array  []int
+		target int
+	}
+	tests := []struct {
+		name string
+		args args
+		want []int
+	}{
+		{
+			name: "",
+			args: args{
+				[]int{1, 2, 3},
+				3,
+			},
+			want: []int{0, 1},
+		},
+
+		{
+			name: "",
+			args: args{
+				[]int{1, 1, 3},
+				2,
+			},
+			want: []int{0, 1},
+		},
+		{
+			name: "",
+			args: args{
+				[]int{2, 7, 11, 15},
+				9,
+			},
+			want: []int{0, 1},
+		},
+		{
+			name: "",
+			args: args{
+				[]int{3, 2, 4},
+				6,
+			},
+			want: []int{1, 2},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := TwoSumSortedArray(tt.args.array, tt.args.target); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("TwoSumSortedArray() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
